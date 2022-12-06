e4b6713fbe97d524b9e87f5968d717e299c4b3eb
naukhaizA <naukhaizanjum251@gmail.com>
2022-12-06 23:30:01 +0500

    Project Completed

--- app/src/main/java/com/example/quranapp/MainActivity.java
+++ app/src/main/java/com/example/quranapp/MainActivity.java
@@ -1,56 +1,39 @@
 package com.example.quranapp;
-
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-import com.example.quranapp.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    Button btn;
+    TextView txtSurah,txtVerse,txtDisplay;
+    QDH qdh = new QDH();
+    QuranArabicText versesObject = new QuranArabicText();
 
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn =findViewById(R.id.btnSearch);
+        txtSurah = findViewById(R.id.txtSurah);
+        txtVerse = findViewById(R.id.txtVerse);
+        txtDisplay = findViewById(R.id.verseDisplay);
+        btn.setOnClickListener(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onClick(View view) {
+        if(R.id.btnSearch == view.getId()){
+            int surahNo = Integer.parseInt(txtSurah.getText().toString());
+            int verseNo = Integer.parseInt(txtVerse.getText().toString());
+            int surahFirstVerse = qdh.getSurahStart(surahNo-1);
+            String finalVerse = versesObject.QuranArabicText[surahFirstVerse + verseNo -1];
+            txtDisplay.setText(finalVerse);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
     }
 }
